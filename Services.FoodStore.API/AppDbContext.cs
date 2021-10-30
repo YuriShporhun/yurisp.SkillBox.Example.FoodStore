33ad62c5fcdd9a1a8ea238fca6b920cc34b05744
Юрий Шпорхун <yuripclive@live.ru>
2021-10-31 02:02:10 +0300

    Add project files.

---
+++ Services.FoodStore.API/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Services.FoodStore.API
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
